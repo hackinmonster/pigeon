4f54f1a7cb994a5365d037c5377d5fa4bb3d9af2
hackinmonster <eric@fackelman.net>
2024-11-02 15:02:55 -0400

    renamed dirs

--- pigeon/settings.py
+++ pigeon/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "project.urls"
+ROOT_URLCONF = "pigeon.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "project.wsgi.application"
+WSGI_APPLICATION = "pigeon.wsgi.application"
 
 
 # Database
--- pigeon/urls.py
+++ pigeon/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from accounts.views import register
 
 urlpatterns = [
-    path("", include("app.urls")),
+    path("", include("core.urls")),
     path("register/", register, name="register"),
     path("admin/", admin.site.urls),
 ]

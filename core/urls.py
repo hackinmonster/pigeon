8b243b740c8bd59c490f4421ba76d879337a497d
hackinmonster <eric@fackelman.net>
2024-11-28 13:57:37 -0500

    added register/login pages, button functionality

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path('register/', views.register, name='register'),
+    path('register/student/', views.student_account, name='student_account'),
+    path('register/employer', views.employer_account, name='employer_account'),
     path('login/', views.login, name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,39 +7,13 @@ def home(request):
     return render(request, 'core/home.html')
 
 def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            user = user_form.save(commit=False)
-            user.set_password(user_form.cleaned_data['password'])
-            user.save()
-            
-            if user.is_student():
-                student_profile_form = StudentProfileForm(request.POST)
-                if student_profile_form.is_valid():
-                    student_profile = student_profile_form.save(commit=False)
-                    student_profile.user = user
-                    student_profile.save()
-            elif user.is_employer():
-                employer_profile_form = EmployerProfileForm(request.POST)
-                if employer_profile_form.is_valid():
-                    employer_profile = employer_profile_form.save(commit=False)
-                    employer_profile.user = user
-                    employer_profile.save()
+    return render(request, 'core/register.html')
 
-            return redirect('login')
-        
-    else:
-        user_form = UserRegistrationForm()
-        student_profile_form = StudentProfileForm()
-        employer_profile_form = EmployerProfileForm()
+def student_account(request):
+    return render(request, 'core/student_account.html')
 
-    return render(request, 'core/register.html', {
-        'user_form': user_form,
-        'student_profile_form': student_profile_form,
-        'employer_profile_form': employer_profile_form,
-    })
-
+def employer_account(request):
+    return render(request, 'core/employer_account.html')
 
 def login(request):
     return render(request, 'core/login.html')
